0740edc66d99a230b3048c9f8c1d46458f7de961
clfarnier <clement.farnier.5@gmail.com>
2022-07-09 18:46:59 +0200

    lancer de dés + résultats

---
+++ src/app/components/dice/dice.component.ts
@@ -0,0 +1,34 @@
+import { Dice } from './../../model/dice.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dice',
+  templateUrl: './dice.component.html',
+  styleUrls: ['./dice.component.css']
+})
+export class DiceComponent implements OnInit {
+
+  diceFaces: number = 0;
+  diceResults: Dice[] = [];
+  totalResult: number = 0;
+  diceToRoll: Dice[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  add() {
+    this.diceToRoll.push({id: Math.floor(Math.random()), faces: this.diceFaces, result: 0});
+  }
+
+  roll() {
+    this.diceResults = [];
+    this.totalResult = 0;
+    this.diceToRoll.forEach(dice => {
+      dice.result = Math.floor(Math.random() * dice.faces) + 1;
+
+      this.diceResults.push(dice);
+      this.totalResult += dice.result;
+    });
+  }
+}
---
+++ src/app/model/dice.model.ts
@@ -0,0 +1,5 @@
+export interface Dice {
+  id: number;
+  faces: number;
+  result: number;
+}
